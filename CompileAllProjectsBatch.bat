66cf441d2cb60f348eee86684d1a36293b537391
Neria Tzidkani <neriat@gmail.com>
2018-07-19 20:45:38 +0300

    Add files via upload

---
+++ CompileAllProjectsBatch.bat
@@ -0,0 +1,4 @@
+go build "Project 07\project7.go"
+go build "Project 08\project8.go"
+go build "Project 10\main\project10.go"
+go build "Project 11\project11.go" "Project 11\symbolTable.go" "Project 11\jackCompiler.go"
